43a2dc2d80869d0282bbb267fb406d108f2d8d57
MaydaySun <19302010001@fudan.edu.cn>
2022-06-13 12:07:46 +0800

    Remove useless annotated SocketIO event handlers

--- Backend/src/main/java/com/example/backend/service/SocketIOService.java
+++ Backend/src/main/java/com/example/backend/service/SocketIOService.java
@@ -1,14 +1,8 @@
 package com.example.backend.service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.annotation.OnConnect;
-import com.corundumstudio.socketio.annotation.OnDisconnect;
-import com.corundumstudio.socketio.annotation.OnEvent;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
 import com.example.backend.model.Column;
 import com.example.backend.model.Plate;
 import com.example.backend.model.Player;
@@ -76,9 +70,7 @@ public class SocketIOService {
             setPlayerPosition(player, data);
 
             // 添加到玩家列表
-            synchronized (room) {
-                room.getPlayers().add(player);
-            }
+            room.getPlayers().add(player);
             Map<String, Object> map = new HashMap<>();
             map.put("player", player);
             // 转发新加入的玩家给其他玩家
@@ -93,18 +85,16 @@ public class SocketIOService {
             UUID uuid = client.getSessionId();
             String username = (String) data.get("username");
             Player player = null;
-            synchronized (room) {
-                for (Player p : room.getPlayers()) {
-                    if (p.getUsername().equals(username))
-                        player = p;
+            for (Player p : room.getPlayers()) {
+                if (p.getUsername().equals(username)) {
+                    player = p;
+                    room.getPlayers().remove(p);
                 }
             }
             if (player == null) {
                 log.info("player is not found in player list");
                 return;
             }
-            // 保存离开前的位置和朝向
-            setPlayerPosition(player, data);
 
             Map<String, Object> map = new HashMap<>();
             map.put("username", username);
@@ -120,11 +110,9 @@ public class SocketIOService {
             UUID uuid = client.getSessionId();
             String username = (String) data.get("username");
             Player player = null;
-            synchronized (room) {
-                for (Player p : room.getPlayers()) {
-                    if (p.getUsername().equals(username))
-                        player = p;
-                }
+            for (Player p : room.getPlayers()) {
+                if (p.getUsername().equals(username))
+                    player = p;
             }
             if (player == null) {
                 log.info("player is not found in player list");
@@ -143,11 +131,9 @@ public class SocketIOService {
             UUID uuid = client.getSessionId();
             String username = (String) data.get("username");
             Player player = null;
-            synchronized (room) {
-                for (Player p : room.getPlayers()) {
-                    if (p.getUsername().equals(username))
-                        player = p;
-                }
+            for (Player p : room.getPlayers()) {
+                if (p.getUsername().equals(username))
+                    player = p;
             }
             if (player == null) {
                 log.info("player is not found in player list");
@@ -178,11 +164,9 @@ public class SocketIOService {
             UUID uuid = client.getSessionId();
             String username = (String) data.get("username");
             Player player = null;
-            synchronized (room) {
-                for (Player p : room.getPlayers()) {
-                    if (p.getUsername().equals(username))
-                        player = p;
-                }
+            for (Player p : room.getPlayers()) {
+                if (p.getUsername().equals(username))
+                    player = p;
             }
             if (player == null) {
                 log.info("player is not found in player list");
@@ -234,202 +218,6 @@ public class SocketIOService {
             socketIOServer = null;
         }
         log.info("socket.io服务已关闭");
-    }
-
-    @OnConnect
-    public void onConnect(SocketIOClient client) {
-        UUID uuid = client.getSessionId();
-        if (uuid != null) {
-            clientMap.put(client.getSessionId(), client);
-            log.info("客户端连接: uuid=" + uuid);
-        }
-    }
-
-    @OnDisconnect
-    public void onDisconnect(SocketIOClient client) {
-        UUID uuid = client.getSessionId();
-        if (uuid != null) {
-            clientMap.remove(uuid);
-            client.disconnect();
-        }
-        log.info("客户端断开连接: uuid=" + uuid);
-    }
-
-    /**
-     * 玩家加入房间的事件处理器
-     */
-    @OnEvent(value = "OnJoin")
-    public void onJoin(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        Player player = new Player();
-        player.setUsername(username);
-        setPlayerPosition(player, data);
-
-        // 添加到玩家列表
-        synchronized (room) {
-            room.getPlayers().add(player);
-        }
-        Map<String, Object> map = new HashMap<>();
-        map.put("player", player);
-        // 转发新加入的玩家给其他玩家
-        sendToOthers("OnPlayerJoin", uuid, map);
-
-        // 向数据库写入日志
-        UserLog userLog = new UserLog(username, "join", LocalDateTime.now(), null);
-        userLogRepository.save(userLog);
-    }
-
-    /**
-     * 玩家离开房间的事件处理器
-     */
-    @OnEvent(value = "OnLeave")
-    public void onLeave(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        Player player = null;
-        synchronized (room) {
-            for (Player p : room.getPlayers()) {
-                if (p.getUsername().equals(username))
-                    player = p;
-            }
-        }
-        if (player == null) {
-            log.info("player is not found in player list");
-            return;
-        }
-        // 保存离开前的位置和朝向
-        setPlayerPosition(player, data);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("username", username);
-        // 转发离开的玩家名给其他玩家
-        sendToOthers("OnPlayerLeave", uuid, map);
-
-        // 向数据库写入日志
-        UserLog userLog = new UserLog(username, "leave", LocalDateTime.now(), null);
-        userLogRepository.save(userLog);
-    }
-
-    /**
-     * 玩家更新位置的事件处理器
-     */
-    @OnEvent(value = "OnUpdate")
-    public void onUpdate(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        Player player = null;
-        synchronized (room) {
-            for (Player p : room.getPlayers()) {
-                if (p.getUsername().equals(username))
-                    player = p;
-            }
-        }
-        if (player == null) {
-            log.info("player is not found in player list");
-            return;
-        }
-        // 更新位置和朝向
-        setPlayerPosition(player, data);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("player", player);
-        // 转发被更新的玩家给其他玩家
-        sendToOthers("OnPlayerUpdate", uuid, map);
-    }
-
-    /**
-     * 玩家拿起汉诺塔的事件处理器
-     */
-    @OnEvent(value = "OnPickUp")
-    public void onPickUp(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        Player player = null;
-        synchronized (room) {
-            for (Player p : room.getPlayers()) {
-                if (p.getUsername().equals(username))
-                    player = p;
-            }
-        }
-        if (player == null) {
-            log.info("player is not found in player list");
-            return;
-        }
-
-        Integer plate = (Integer) data.get("index");
-        player.setPlate(plate);
-
-        Integer columnIndex = (Integer) data.get("columnIndex");
-        Column column = room.getColumns().get(columnIndex);
-        // 更新柱子的plates
-        synchronized (room.getColumns().get(columnIndex)) {
-            column.getPlates().remove(0);
-        }
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("username", username);
-        map.put("index", plate);
-        // 转发被更新的玩家给其他玩家
-        sendToOthers("OnPlayerPickUp", uuid, map);
-        // 向数据库写入日志
-        UserLog userLog = new UserLog(username, "pickUp", LocalDateTime.now(), plate);
-        userLogRepository.save(userLog);
-    }
-
-    /**
-     * 玩家放下汉诺塔的事件处理器
-     */
-    @OnEvent(value = "OnPutDown")
-    public void onPutDown(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        Player player = null;
-        synchronized (room) {
-            for (Player p : room.getPlayers()) {
-                if (p.getUsername().equals(username))
-                    player = p;
-            }
-        }
-        if (player == null) {
-            log.info("player is not found in player list");
-            return;
-        }
-
-        Integer plateIndex = (Integer) data.get("index");
-        Plate plate = new Plate(plateIndex, Plate.BASE_RADIUS + plateIndex * Plate.RADIUS_STEP, Plate.HEIGHT);
-        player.setPlate(null);
-
-        Integer columnIndex = (Integer) data.get("columnIndex");
-        Column column = room.getColumns().get(columnIndex);
-        // 更新柱子的plates
-        synchronized (room.getColumns().get(columnIndex)) {
-            column.getPlates().add(0, plate);
-        }
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("username", username);
-        map.put("index", plateIndex);
-        map.put("columnIndex", columnIndex);
-        // 转发给其他玩家
-        sendToOthers("OnPlayerPutDown", uuid, map);
-        // 向数据库写入日志
-        UserLog userLog = new UserLog(username, "putDown", LocalDateTime.now(), null);
-        userLogRepository.save(userLog);
-    }
-
-    /**
-     * 玩家发送聊天消息的事件处理器
-     */
-    @OnEvent(value = "OnSendMessage")
-    public void onSendMessage(SocketIOClient client, AckRequest ackRequest, JSONObject data) {
-        UUID uuid = client.getSessionId();
-        String username = (String) data.get("username");
-        String message = client.getHandshakeData().getSingleUrlParam("message");
-        Map<String, Object> map = new HashMap<>();
-        map.put("username", username);
-        map.put("message", message);
-        sendToOthers("OnPlayerSendMessage", uuid, map);
     }
 
     private void setPlayerPosition(Player player, JSONObject data) {
